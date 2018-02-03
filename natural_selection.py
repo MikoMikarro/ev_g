22de515f496b7672ddb2b68bb739a6716a34ffe2
Alfrodo21 <fredikayak@gmail.com>
2018-02-03 14:02:14 +0100

    Secon commit

--- natural_selection.py
+++ natural_selection.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 d = "/"
 
 gen = 8
-n_alelos = 12
+n_alelos = 2
 n_individuos_alelo = 5
 
 file_name = "data.txt"
@@ -17,11 +17,11 @@ def iniciar(n_alelos,n_individuos_alelo):
     for i in range(n_alelos):
         for l in range(n_individuos_alelo):
             pob.append(i+1)
-
     lines = []
+    text = ""
     file = open(file_name,"w")
     for i in range(n_alelos):
-        text += str(i+1)+":"+str(n_individuos_alelo)
+        text += str(i+1)+":"+str(n_individuos_alelo)+","
     text += "\n"
     file = open(file_name, "w")
     file.write(text)
@@ -49,11 +49,11 @@ def evolve():
     file.write(data_prev+text)
     file.close()
 
-def representar():
+#def representar():
 
 
 
 iniciar(n_alelos,n_individuos_alelo)
 for i in range(gen):
     evolve()
-representar()
+#representar()

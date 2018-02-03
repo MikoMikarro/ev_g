d1b01b30b5cf42fcff185d4f4b8a42265253e3f0
Alfrodo21 <fredikayak@gmail.com>
2018-02-03 22:32:39 +0100

    Almost finished

--- natural_selection.py
+++ natural_selection.py
@@ -6,13 +6,11 @@ import matplotlib.pyplot as plt
 
 d = "/"
 
-generacion = 8
-n_alelos = 5
-n_individuos_alelo = 10
 
 file_name = "data.txt"
 
 pob = []
+
 
 while True:
     n_alelos = input("Alelos:")
@@ -73,7 +71,7 @@ def representar():
     file.close()
 
     pob_rep = []
-    n_generaciones = generaciones + 1
+    n_generaciones = len(data)
 
     for i in range(n_alelos):
         pob_rep.append([])
@@ -83,12 +81,56 @@ def representar():
             pob_rep[l].append(int(split(split(i,",")[l],":")[1]))
 
     for i in pob_rep:
-        plt.plot(range(n_generaciones),i)
+        plt.plot(range(n_generaciones),i,".-")
 
     plt.show()
-
-
+#---------------------------#
 iniciar(n_alelos,n_individuos_alelo)
 for i in range(generaciones):
     evolve()
 representar()
+
+check_1 = True
+while check_1:
+    print "Opciones:"
+    print "A -> Anhadir mas generaciones."
+    print "B -> Seguir hasta que solo queden n con vida."
+    print "C -> Salir."
+    resp = raw_input()
+
+    if resp.upper() == "A" :
+        check_2 = True
+        while check_2:
+            ad_generaciones = input("Numero de generaciones adicionales:")
+            for i in range(ad_generaciones):
+                evolve();
+            representar()
+            check_2 = False
+
+    elif resp.upper() == "B" :
+        check_3 = True
+        while check_3:
+            resp_2 = input("Alelos supervivientes:")
+            while True:
+                if resp_2 <= n_alelos and resp_2 >=1:
+                    break
+                else:
+                    print "Error"
+            check_4 = True
+            while check_4:
+                evolve()
+                file = open(file_name,"r")
+                data = file.readlines()[-1]
+                file.close()
+                pob_prev = []
+                for i in split(data,",")[:-1]:
+                    for l in range(int(split(i,":")[1])):
+                        pob_prev.append(split(i,":")[0])
+
+                n = pob_prev.count("0")
+
+                if n >= resp_2 :
+                    representar()
+                    check_4 = False
+                    check_3 = False
+                    break
